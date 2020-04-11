5a0cfd4c9fbc2240027af1e32adeb8a94e86c3f2
Juan Patricio Di Bacco <jpdibacco@gmail.com>
2020-04-11 05:47:01 +0200

    added particlesjs

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 import 'bootstrap';
 import './scss/app.scss';
 import 'particles.js';
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'js/vendor/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
